1772d6605ce5692c492cd597c3456df6b76657fc
oublie6 <47771412+oublie6@users.noreply.github.com>
2026-07-26 10:37:43 +0800

    feat: support environment injected production secrets

---
+++ server/apps/app-api/internal/config/environment.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func (c *Config) ApplyEnvironment() error {
+	applyString("APP_HOST", &c.Host)
+	applyString("APP_MYSQL_ADDR", &c.MySQL.Addr)
+	applyString("APP_MYSQL_DATABASE", &c.MySQL.Database)
+	applyString("APP_MYSQL_USER", &c.MySQL.User)
+	applyString("APP_MYSQL_PASSWORD", &c.MySQL.Password)
+	applyString("APP_REDIS_ADDR", &c.Redis.Addr)
+	applyString("APP_REDIS_USERNAME", &c.Redis.Username)
+	applyString("APP_REDIS_PASSWORD", &c.Redis.Password)
+	applyString("APP_AUTH_ACCESS_TOKEN_SECRET", &c.Authentication.AccessTokenSecret)
+
+	if raw := strings.TrimSpace(os.Getenv("APP_PORT")); raw != "" {
+		port, err := strconv.Atoi(raw)
+		if err != nil {
+			return fmt.Errorf("parse APP_PORT: %w", err)
+		}
+		c.Port = port
+	}
+	return nil
+}
+
+func applyString(name string, target *string) {
+	if value := strings.TrimSpace(os.Getenv(name)); value != "" {
+		*target = value
+	}
+}
